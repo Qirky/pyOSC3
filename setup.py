7007ed0452a2722344cf1c73ab9c6ae2a198f5fc
Ryan Kirkbride <ryankirkbride26@gmail.com>
2020-07-11 10:23:01 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pyOSC3',
-      version="1.1",
+      version="1.2",
       description='pyOSC updated for Python 3',
       author='Ryan Kirkbride',
       author_email='ryan@foxdot.org',
