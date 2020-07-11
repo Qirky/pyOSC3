0806cf5308fc1613b78d1d808753e9fef7a55e4e
Ryan Kirkbride <ryankirkbride26@gmail.com>
2020-07-11 10:20:38 +0100

    Update OSC3.py based on FoxDot updates. Allows printing OSC messages

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pyOSC3',
+      version="1.1",
+      description='pyOSC updated for Python 3',
+      author='Ryan Kirkbride',
+      author_email='ryan@foxdot.org',
+      url='http://foxdot.org/',
+      packages=['pyOSC3'] )
+
+
